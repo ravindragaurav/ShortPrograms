620ce7de69273808d3ed4b2d96f82b4df059ddac
Gaurav Ravindra <ravindra_gaurav@yahoo.com>
2017-07-15 16:07:12 +0100

    TDD for array median

---
+++ src/ArrayMedian/Logic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMedian
+{
+    class Logic
+    {
+        public int[] SortArray(int[] unsortedArray)
+        {
+            Array.Sort(unsortedArray);
+            return unsortedArray;
+        }
+
+        public int ReturnLength(int[] unsortedArray)
+        {
+            return unsortedArray.Length; 
+        }
+
+        public double FindMedian(int[] array)
+        {
+            array = SortArray(array);
+
+            if(ReturnLength(array) % 2 != 0 )
+            {
+                return array[(ReturnLength(array) / 2)];
+            }
+            else
+            {
+                var middle1 = ReturnLength(array)/2 - 1;
+                var middle2 = ReturnLength(array)/2;
+
+                return (array[middle1] + array[middle2]) / 2.0;
+            }
+        }
+    }
+}
---
+++ src/ArrayMedian/LogicTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+
+namespace ArrayMedian
+{
+    class LogicTests
+    {
+
+        private Logic _logic;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _logic = new Logic();
+        }
+
+        [Test]
+        public void Should_Sort_Array_Ascending()
+        {
+            //given
+            int[] unsortedArray = new int[] { 5, 6, 2, 7, 1 };
+
+            //When
+            var result = _logic.SortArray(unsortedArray);
+
+            //Then
+            Assert.That(result[0], Is.EqualTo(1));
+            Assert.That(result[4], Is.EqualTo(7));
+        }
+
+        [Test]
+        public void Should_Return_Array_Length()
+        {
+            //given
+            int[] array = new int[] { 5, 6, 2, 7, 1 };
+
+            //When
+            var result = _logic.ReturnLength(array);
+
+            //Then
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Should_Return_Array_Median_For_Odd_Number_Of_Elements()
+        {
+            //given
+            int[] array = new int[] {1,2,3,4,5};
+
+            //When
+            var result = _logic.FindMedian(array);
+
+            //Then
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Should_Return_Array_Median_For_Even_Number_Of_Elements()
+        {
+            //given
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            //When
+            double result = _logic.FindMedian(array);
+
+            //Then
+            Assert.That(result, Is.EqualTo(3.5));
+        }
+
+        [Test]
+        public void Should_Return_Array_Median_For_Unsorted_Array()
+        {
+            //given
+            int[] array = new int[] { 5,3,6,2,4,1 };
+
+            //When
+            double result = _logic.FindMedian(array);
+
+            //Then
+            Assert.That(result, Is.EqualTo(3.5));
+        }
+
+
+    }
+}
